3d4a46cefdea143280b49677f7ff514ec2816557
agarwood <676786620@qq.com>
2022-01-29 12:03:59 +0800

    style:chat toolbar style

--- src/app/pages/chat/chat-toolbar/chat-toolbar.component.ts
+++ src/app/pages/chat/chat-toolbar/chat-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chat-toolbar',
@@ -6,18 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-toolbar.component.scss']
 })
 export class ChatToolbarComponent implements OnInit {
+  /**
+   * height of chat toolbar
+   */
+  @Input()
+  public chatToolbarHeight: number = 0;
+
+  /**
+   * height of chat toolbar header
+   */
+  public chatToolbarHeaderHeight: number = 54;
+
+  /**
+   * height of chat toolbar content
+   */
+  public tabContentHeight: number = 0;
+
   public constructor () {
     console.log('height');
   }
 
-  tab1acticeID: string | number = 'tab2';
-  tab2acticeID: string | number = 'tab3';
+  /**
+   * product items by recommend.
+   */
+  public tabProduct: string | number = 'tabProduct';
 
+  /**
+   * order item
+   */
+  public tabOrder: string | number = 'tabOrder';
+
+  /**
+   * quick reply item
+   */
+  public tabQuick: string | number = 'tabQuick';
+
+  /**
+   * ngOnInit
+   */
   public ngOnInit (): void {
-    console.log('ChatToolbarComponent');
+    this.setToolbarHeight();
   }
 
-  activeTabChange (id) {
+  /**
+   * change tab
+   *
+   * @param id
+   */
+  public activeTabChange (id: string | number): void {
     console.log(id);
+  }
+
+  /**
+   * Setting Sidebar Height
+   */
+  @HostListener('window:resize', ['$event'])
+  public setToolbarHeight (): void {
+    setTimeout(() => {
+      this.tabContentHeight = this.chatToolbarHeight - this.chatToolbarHeaderHeight;
+    }, 100);
   }
 }
